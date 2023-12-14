c7802456af7ce71ad74851fcec6707d6b00f9909
PRIYAANAVEKAR <priyanavekar@gmail.com>
2023-12-14 14:13:51 +0530

    created the new example

---
+++ src/Git/Example.java
@@ -0,0 +1,22 @@
+package Git;
+
+public class Example {
+	private String name;
+	private int idl;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getIdl() {
+		return idl;
+	}
+	public void setIdl(int idl) {
+		this.idl = idl;
+	}
+	
+	
+	
+
+}
