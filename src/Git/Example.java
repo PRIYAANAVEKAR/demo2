aca89f1cd519e7ce1adc07676e229988bfa300d3
PRIYAANAVEKAR <priyanavekar@gmail.com>
2023-12-14 14:35:10 +0530

    added adress

--- src/Git/Example.java
+++ src/Git/Example.java
@@ -3,6 +3,14 @@ package Git;
 public class Example {
 	private String name;
 	private int idl;
+	private String address;
+	
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
 	public String getName() {
 		return name;
 	}
